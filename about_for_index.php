be418bfebe3086fbefbce99196e3d399f569becd
Samyak <samyakghagargunde66@gmail.com>
2022-08-18 18:17:53 +0530

    initial commit

---
+++ about_for_index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>iBlog - Heaven for bloggers</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="/">
+            <span><img src="img/logo.png" width="94px" alt=""></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">login</a>
+                </li>    
+            
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Contact</a>
+                </li>
+
+                
+                <li class="nav-item active">
+                    <a class="nav-link" href="about_for_index.php">About <span class="sr-only">(current)</span></a>
+                </li>
+                
+
+                
+            </ul>
+
+
+        </div>
+    </nav>
+
+<div class="container mt-4">
+<h3>Introduction Here:</h3>
+<hr>
---
+++ blogpost.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/utils.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/blogpost.css">
+    <link rel="stylesheet" href="css/mobile.css">
+    <title>iBlog - Heaven for bloggers</title>
+</head>
+<body>
+<nav class="navigation max-width-1 m-auto">
+        <div class="nav-left">
+            <a href="welcome.php">
+                <span><img src="img/logo.png" width="94px" alt=""></span>
+            </a>
+            <ul>
+                <li><a href="welcome.php">Home</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+        <div class="navbar-collapse collapse">
+  
+  </div>
+        <div class="nav-right">
+            <form action="search.php" method="get">
+                <input class="form-input" type="text" name="query" placeholder="Article Search">
+                <button class="btn">Search</button>
+            </form>
+
+        </div>
+    </nav>
+    <div class="max-width-1 m-auto">
+        <hr>
+    </div>
+    <div class="post-img">
+        <img src="img/11.svg" alt="">
+    </div>
+    <div class="m-auto blog-post-content max-width-2 m-auto my-2">
+        <h1 class="font1">The heaven for bloggers</h1>
+        <div class="blogpost-meta">
+            <div class="author-info">
+                <div>
+                <b>
+                  Author - Samyak Ghagargunde
+                </b>
+                </div>
+                <div>04 January. 6 min read</div>
+            </div>
+            <div class="social">
+                <svg width="29" height="29" class="hk"><path d="M22.05 7.54a4.47 4.47 0 0 0-3.3-1.46 4.53 4.53 0 0 0-4.53 4.53c0 .35.04.7.08 1.05A12.9 12.9 0 0 1 5 6.89a5.1 5.1 0 0 0-.65 2.26c.03 1.6.83 2.99 2.02 3.79a4.3 4.3 0 0 1-2.02-.57v.08a4.55 4.55 0 0 0 3.63 4.44c-.4.08-.8.13-1.21.16l-.81-.08a4.54 4.54 0 0 0 4.2 3.15 9.56 9.56 0 0 1-5.66 1.94l-1.05-.08c2 1.27 4.38 2.02 6.94 2.02 8.3 0 12.86-6.9 12.84-12.85.02-.24 0-.43 0-.65a8.68 8.68 0 0 0 2.26-2.34c-.82.38-1.7.62-2.6.72a4.37 4.37 0 0 0 1.95-2.51c-.84.53-1.81.9-2.83 1.13z"></path></svg>
+
+                <svg style="background: black;
+                border-radius: 21px;" width="29" height="29" viewBox="0 0 29 29" fill="none" class="hk"><path d="M5 6.36C5 5.61 5.63 5 6.4 5h16.2c.77 0 1.4.61 1.4 1.36v16.28c0 .75-.63 1.36-1.4 1.36H6.4c-.77 0-1.4-.6-1.4-1.36V6.36z"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M10.76 20.9v-8.57H7.89v8.58h2.87zm-1.44-9.75c1 0 1.63-.65 1.63-1.48-.02-.84-.62-1.48-1.6-1.48-.99 0-1.63.64-1.63 1.48 0 .83.62 1.48 1.59 1.48h.01zM12.35 20.9h2.87v-4.79c0-.25.02-.5.1-.7.2-.5.67-1.04 1.46-1.04 1.04 0 1.46.8 1.46 1.95v4.59h2.87v-4.92c0-2.64-1.42-3.87-3.3-3.87-1.55 0-2.23.86-2.61 1.45h.02v-1.24h-2.87c.04.8 0 8.58 0 8.58z" fill="#fff"></path></svg>
+
+                <svg width="29" height="29" class="hk"><path d="M23.2 5H5.8a.8.8 0 0 0-.8.8V23.2c0 .44.35.8.8.8h9.3v-7.13h-2.38V13.9h2.38v-2.38c0-2.45 1.55-3.66 3.74-3.66 1.05 0 1.95.08 2.2.11v2.57h-1.5c-1.2 0-1.48.57-1.48 1.4v1.96h2.97l-.6 2.97h-2.37l.05 7.12h5.1a.8.8 0 0 0 .79-.8V5.8a.8.8 0 0 0-.8-.79"></path></svg>
+
+            </div>
+        </div>
+         <p class="font1">What is SCADA?
+            SCADA stands for supervisory control and data acquisition, it is a control system architecture that consists of networked data communications, graphical user interfaces, and computers for high-level process supervisory management.
+            
+            It also consists of various other peripheral devices like discrete proportional integral derivative and programmable logic controllers which are known for interfacing with process machinery and plant. SCADA systems are used for operating and managing project-driven processes in construction.
+            
+            The SCADA computer system is known for handling various operator interfaces which are known for enabling the issuing and monitoring of process commands like controller setpoint changes.
+            
+            The real-time control logic or controller calculations which are an example of subordinated operations are performed by the networked modules which are connected to the field actuators and sensors.
+            
+            The concept of SCADA has become a universal means of remote access to various local control modules as it was developed in this way. The manufacturers of SCADA are different and are known for allowing access via standard automation protocol.
+            
+            When in practice, the huge SCADA systems have grown up to convert into the same kind of distributed control systems that are in function. These are known to control for controlling the large-scale processes which comprise numerous sites. These are capable of working over large distances as well as small distances.
+            
+            SCADA System Components
+            A SCADA system comprises various essential elements such as supervisory computers, programmable logic controllers, remote terminal units, human-machine interface, and communication infrastructure. Lets read about these SCADA system components in detail.</p>
+         <p class="font1">Supervisory Computers:
+
+            Supervisory computers form the core of SCADA systems and are known for gathering data on the process. These are known for transferring the control commands to the field-connected devices. These refer to the software and computer that are responsible for communicating with the controllers that connect the field.
+            
+            Programmable Logic Controllers:
+            
+            Programmable logic controllers, commonly known as PLCs are connected to actuators and sensors in the process. These are known for being networked to the supervisory system. When it comes to factory automation, these have a connection of high speed to the SCADA systems
+            
+            In remote applications like a large water treatment plant, these can connect directly to SCADA via a wireless link. These are specially designed for controlling and acted as a founding platform for programming languages.
+            
+            Remote Terminal Units:
+            
+            RTU or remote terminal units connected to the actuators and sensors in the process. These are connected to the supervisory computer system through a network. These have control capabilities that are embedded and mostly conform to the programming standard and support automation.
+            
+            Human-Machine Interface:
+            
+            HMI or human-machine interface acts as an operator window of the supervisory system. It is known for presenting the information of plants to the operating personnel in a graphical manner. These graphical diagrams comprise mimic diagrams which are the schematic representation of the plant which is being controlled, event logging pages, and alarm.
+            
+            The human-machine interface is linked to the SCADA supervisory computer to drive mimic diagrams, trending graphs, and alarm displays by providing live data. The HMI acts as a graphical user interface for the operators in numerous installations. It collects the data from external devices, sends notifications, performs alarms, and creates reports.
+            
+            Communication Infrastructure:
+            
+            The communication infrastructure is known for connecting the supervisory computer system to the PLCs and RTUs. They may utilize manufacturer proprietary protocols or industry standards but both PLCs and RTUs are known for their autonomous functioning on the near-real-time process control. </p>
+         <p class="font1">Types of SCADA
+            The SCADA systems are of four types are Monolithic SCADA system, Distributed SCADA system, Networked SCADA system, and IoT SCADA system. Lets read about these types of SCADA in detail.
+            
+            Monolithic SCADA System:
+            
+            These systems are the first-generation systems that make use of minicomputers. These systems may be developed in the absence of commonly used network services. These systems may be designed as independent systems without any specific relation to the other.
+            
+            Distributed SCADA System:
+            
+            Distributed SCADA systems are the second-generation systems; the control functions may be distributed across various systems by joining them with LAN or local area network. By sharing command processing and real-time data, the control operations may be performed.
+            
+            The cost and size of each station get reduced in these types of systems. However, it is to be noted that they dont have any consistent protocols as such. Very few people understood the SCADA system security as the protocols were considered proprietary.
+            
+            Networked SCADA System:
+            
+            These systems are known as the third-generation systems; the communication and network of the present SCADA systems may be done by utilizing the WAN system via phones or data lines. The transmission of two nodes may be done by taking the help of Fiber-optic connections or Ethernet.
+            
+            IoT SCADA System:
+            
+            IoT SCADA systems are the fourth-generation systems that have reduced cost as a result of IoT implementation via cloud computing. The maintenance of these systems is considered easy in comparison to the others.
+            
+            In real-time, the condition of these systems may be reported via cloud computing. This is why the implementation of the algorithms comprising of intricate control may be performed that are generally used on usual PLCs.</p>
+         <p class="font1">Examples of SCADA Use
+            SCADA systems are used in various industries, especially in places where it is tough to have a human presence due to a lack of security. This may be a result of various factors that contribute to it. This comprises a facility in a remote area that is tough to reach on time.
+            
+            Lets read about the examples of SCADA use. A general SCADA network may be set up for monitoring leakages in a pipeline. Once the leak is detected, the system carries on pre-configured commands.
+            
+            The devices which carry on these functions are either remote terminal unit (RTU) or programmable logic controller (PLC). The remote terminal unit is generally considered as the device of choice as it brings with it more control and automation features for the management system.
+            
+            Applications of SCADA System 
+            The application of the SCADA system is done in different industries in various forms. Lets read about the industries in detail.
+            
+            Electric power generation, transmission, and distribution: The electric utilities make use of RTI, PLC, and HMI SCADA for detecting the flow of current and voltage lines of remote sites. These are used for monitoring the operation of breakers and for taking power grids on and off. SCADA systems may be found at the electrical power distribution from coal, nuclear, electrical power distribution and transmission, gas-fired coal, etc.
+            
+            Water, wastewater, and sewage: The municipal water and state utilize SCADA applications. These are known for monitoring and regulating reservoir levels, water flow, wastewater collection and treatment facilities, pipe pressure, water treatment distribution, and centers and different factors of the field devices.
+            
+            Buildings, facilities, and environments: The facility managers are known for using SCADA for monitoring and controlling HVAC, refrigeration units, lighting, temperature sensors, and entry systems.
+            
+            Manufacturing: The other application of SCADA includes managing part list for manufacturing just-in-time and regulating industrial robots and automation. It is used for monitoring the quality and controlling the process in industrial plants.
+            
+            Mass Transit:
+            
+            The SCADA protocols are used by the transit authorities for regulating the electricity in remote areas. It is also used for automating traffic signals for tracking and locating buses and trains and for controlling the railroad crossing gates.
+            
+            Traffic Signals:
+            
+            SCADA is used for controlling traffic flow, regulating traffic lights, and detecting out-of-order signals.
+            
+            Oil and Gas:
+            
+            SCADA software application is used for remotely monitoring and controlling the equipment which is related to pumps, storage, pipelines, onshore wells, offshore platforms, refineries, and petro-chemical stations.
+            
+            Food and Beverage Production:
+            
+            It is very important to keep the temperature within an adequate range while making and processing food. These systems are used for monitoring and controlling temperatures within these facilities.</p>
+    </div>
+
+    <div class="max-width-1 m-auto"><hr></div>
+    <div class="home-articles max-width-1 m-auto font2">
+        <h2>People who read this also read</h2>
+        <div class="row">
+
+       
+        <div class="home-article more-post">
+            <div class="home-article-img">
+                <img src="img/11.svg" alt="article">
+            </div>
+            <div class="home-article-content font1 center">
+                <a href="blogpost.php"><h3>Learn more about Machine Learning techniques in India by joining this channel</h3></a>
+                
+                <div>Author Name</div>
+                <span>07 January | 6 min read</span>
+            </div>
+        </div>
+        <div class="home-article more-post">
+            <div class="home-article-img">
+                <img src="img/1.png" alt="article">
+            </div>
+            <div class="home-article-content font1 center">
+                <a href="blogpost.php"><h3>Learn more about Machine Learning techniques in India by joining this channel</h3></a>
+                
+                <div>Author Name</div>
+                <span>07 January | 6 min read</span>
+            </div>
+        </div>
+        <div class="home-article more-post">
+            <div class="home-article-img">
+                <img src="img/2.png" alt="article">
+            </div>
+            <div class="home-article-content font1 center">
+                <a href="blogpost.php"><h3>Learn more about Machine Learning techniques in India by joining this channel</h3></a>
+                
+                <div>Author Name</div>
+                <span>07 January | 6 min read</span>
+            </div>
+        </div>
+        
+    </div>
+    </div>
+
+    <div class="footer">
+        <p>Copyright &copy; iBlog.com </p>
+         <a href="https://www.vecteezy.com/free-vector/typewriter">Vector Credits: Vecteezy</a> 
+    </div>
+</body>
+</html>
